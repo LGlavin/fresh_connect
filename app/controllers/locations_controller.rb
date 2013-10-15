b2483c53ddb34449e83d9e4152c9383d20e5d84a
Elizabeth Glavin <eglavin@gmail.com>
2013-10-15 08:27:45 -0400

    updated location controller

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,25 +1,35 @@
 class LocationsController < ApplicationController
   
   def index
-    if params[:search].present?
-     @locations = Locations.near(params[:search], 100, :order => :distance)
+    @locations = if search_value.present?
+                  Location.near(search_value)
+                else
+                  Location.all
+                end
+              end
+            
+
+
+  #   if params[:search].present?
+  #    @locations = Locations.near(params[:search], 100, :order => :distance)
       
-    else
-      @locations = Location.all
-    end
- end   
+  #   else
+  #     @locations = Location.all
+  #   end
+  # end 
+  
    private
     def search_value
       params[:search] && params[:search][:value]
     end
 
-  
-  def new
-    @location = Location.new
-  end
+  # def new
+  #   @location = Location.new
+  # end
 
-  def show
-    @location = Location.find(params[:id])
-  end
- end
+  # def show
+  #   @location = Location.find(params[:id])
+  # end
+end
+
 
--- app/models/location.rb
+++ app/models/location.rb
@@ -8,13 +8,13 @@ private
 
 def geocode_by_postal_code
   self.latitude, self.longitude = PostalCode.new(postal_code).coordinates
-
+end
 
   acts_as_gmappable
 
-def gmaps4rails_address
-#describe how to retrieve the address from your model, if you use directly a db column, you can dry your code, see wiki
-  "#{self.street}, #{self.city}, #{self.country}" 
-end
+  def gmaps4rails_address
+  #describe how to retrieve the address from your model, if you use directly a db column, you can dry your code, see wiki
+    "#{self.street}, #{self.city}, #{self.country}" 
   end
+ end
 end
