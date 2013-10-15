a8396f7b69fa39fc2bba07f69151750cb5eca275
Elizabeth Glavin <eglavin@gmail.com>
2013-10-14 20:49:48 -0400

    fixed homepage image

--- app/models/market.rb
+++ app/models/market.rb
@@ -3,10 +3,16 @@ class Market < ActiveRecord::Base
   validates_presence_of :name
   validates_presence_of :address
   validates_presence_of :datetime
-  validates_presence_of :city
-  validates_presence_of :postal_code
-  validates_presence_of :state
+
 
   geocoded_by :address
 after_validation :geocode, :if => :address_changed?
+
+
+  acts_as_gmappable :process_geocoding => false
+
+def gmaps4rails_address
+#describe how to retrieve the address from your model, if you use directly a db column, you can dry your code, see wiki
+  "#{self.street}, #{self.city}, #{self.country}" 
+end
 end
