85e534b5c332341da03671665c80c87ea100618d
Elizabeth Glavin <eglavin@gmail.com>
2013-10-15 07:52:36 -0400

    changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,30 +6,30 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-require 'csv'
+# require 'csv'
 
-CSV.foreach("db/markets.csv", headers:true) do |row| 
+# CSV.foreach("db/markets.csv", headers:true) do |row| 
 
-    market = markets.where({
-      name: row[1],
-      address: row[2],
-      city: row[3],
-      postal_code: row[4].gsub(/\D/, "").to_i,
-      latitude: row[5].gsub(/\D/, "").to_i,
-      longitude: row[6].gsub(/\D/, "").to_i
-      }).first
+#     market = markets.where({
+#       name: row[1],
+#       address: row[2],
+#       city: row[3],
+#       postal_code: row[4].gsub(/\D/, "").to_i,
+#       latitude: row[5].gsub(/\D/, "").to_i,
+#       longitude: row[6].gsub(/\D/, "").to_i
+#       }).first
   
-    if market.nil?
-      market = markets.new ({
-      name: row[1],
-      address: row[2],
-      city: row[3],
-      postal_code: row[4].gsub(/\D/, "").to_i,
-      latitude: row[5].gsub(/\D/, "").to_i,
-      longitude: row[6].gsub(/\D/, "").to_i
-      })
+#     if market.nil?
+#       market = markets.new ({
+#       name: row[1],
+#       address: row[2],
+#       city: row[3],
+#       postal_code: row[4].gsub(/\D/, "").to_i,
+#       latitude: row[5].gsub(/\D/, "").to_i,
+#       longitude: row[6].gsub(/\D/, "").to_i
+#       })
         
-        market.save!
-      end
-  end
+#         market.save!
+#       end
+#   end
 
