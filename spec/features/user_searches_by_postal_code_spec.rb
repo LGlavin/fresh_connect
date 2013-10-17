ac77e022d3227760e48e265cc488e85a2ca437ff
Elizabeth Glavin <eglavin@gmail.com>
2013-10-17 08:43:53 -0400

    tests for geocode not green thou

--- spec/features/user_searches_by_postal_code_spec.rb
+++ spec/features/user_searches_by_postal_code_spec.rb
@@ -7,7 +7,16 @@ So I know when they are occuring
 } do 
 
   scenario 'display locations from the search' do
-    
+    stub_geocoding_request 'Scituate, MA'
+    stub_geocoding_request 'Boston, Ma', 42.35548199999999, -71.0608386
+  
+    scituate_location = create(:location, :in_scituate)
+    boston_location = create(:location, :in_boston)
+
+    locations = LocationsOnPage.new
+    locations.search boston_location.postal_code
+
+    expect(locations).to include(boston_location)
   end 
   
 end
