2ca88099259f43d53b4a153ab49790f597074abe
Elizabeth Glavin <eglavin@gmail.com>
2013-10-18 11:59:59 -0400

    production update

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,7 +15,7 @@ FreshConnect::Application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
-  
+  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
 
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
