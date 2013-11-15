7924b56af2061f5807fb7a82c88cc72f434676be
Elizabeth Glavin <eglavin@gmail.com>
2013-11-15 18:38:46 -0500

    update production file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,14 +21,13 @@ FreshConnect::Application.configure do
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = true
-     config.assets.initialize_on_precompile=falseßß
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs.
   config.assets.digest = true
