93329df1c1624f9c969caf0d273f265c3e5af531
Elizabeth Glavin <eglavin@gmail.com>
2013-11-18 10:39:58 -0500

    longitude validations

--- app/models/market.rb
+++ app/models/market.rb
@@ -1,8 +1,8 @@
 class Market < ActiveRecord::Base
   belongs_to :user
   validates_presence_of :name
-  validates_presence_of :longitude
-  validates_presence_of :latitude
+  #validates_presence_of :longitude
+  #validates_presence_of :latitude
   #validates_presence_of :address
   #validates_presence_of :datetime
 
