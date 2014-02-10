21ae3f80e7142398098a52fc8fbc93a1a70ac0f8
Elizabeth Glavin <eglavin@gmail.com>
2014-02-10 16:28:09 -0500

    css3 title

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,11 @@
-# class UsersController < ApplicationController
+class UsersController < ApplicationController
 
-#   def show
-#     @user = User.find(params[:id])
-#   end
+  def show
+    @user = User.find(params[:id])
+  end
 
-#   def user_params
-#     params.require(:user).permit(:user_name, :last_name, :first_name)
-#   end
+  def user_params
+    params.require(:user).permit(:user_name, :last_name, :first_name)
+  end
 
-# end
+end
