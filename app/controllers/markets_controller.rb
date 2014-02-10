93c2942744de9d8d37f07ca405194f126b0cb0e2
Elizabeth Glavin <eglavin@gmail.com>
2014-02-10 11:32:43 -0500

    paginate working with reg search

--- app/controllers/markets_controller.rb
+++ app/controllers/markets_controller.rb
@@ -2,7 +2,7 @@ class MarketsController < ApplicationController
   
   def index
       if params[:search].present?
-      @markets = Market.near(params[:search], 15).page(params[:page]).per(10)
+      @markets = Market.near(params[:search]).page(params[:page]).per(10)
    else
     @markets = Market.all.page(params[:page]).per(10)
   end
