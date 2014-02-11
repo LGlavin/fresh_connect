8222d6cee9dafc0bc23f9cfb7807c98553ffc990
Elizabeth Glavin <eglavin@gmail.com>
2014-02-11 15:48:48 -0500

    this may work

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,7 @@ module FreshConnect
       config.assets.precompile += [
       'market.css.scss'
     ]
+    config.assets.precompile += %w( vendor/custom.modernizr )
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
