02efe899b8ff9a513ef69ddbbdd78c4aac6f0b1d
Elizabeth Glavin <eglavin@gmail.com>
2014-02-11 13:26:18 -0500

    applicaiton.rb

--- config/application.rb
+++ config/application.rb
@@ -7,6 +7,10 @@ Bundler.require(:default, Rails.env)
 
 module FreshConnect
   class Application < Rails::Application
+      config.assets.precompile += [
+      'market.css'
+    ]
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
